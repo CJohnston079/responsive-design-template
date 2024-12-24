9bb7107b270805465b6a4406900b3173181120a7
Chris <chris79gp@outlook.com>
2024-12-24 20:47:45 +0000

    Feat: hide nav elements from accessibiliy tree when nav menu is closed

--- main.js
+++ main.js
@@ -4,9 +4,29 @@ console.log("Hello world!");
 
 const btnOpen = document.querySelector("#btnOpen");
 const btnClose = document.querySelector("#btnClose");
+const menuTopNav = document.querySelector("#menuTopNav");
+const breakpoint = window.matchMedia("(width < 43.75em)");
 
-const openMobileMenu = () => btnOpen.setAttribute("aria-expanded", "true");
-const openCloseMenu = () => btnOpen.setAttribute("aria-expanded", "false");
+const setupTopNav = () => {
+	if (breakpoint.matches) {
+		menuTopNav.setAttribute("inert", "");
+	} else {
+		menuTopNav.removeAttribute("inert");
+	}
+};
+
+const openMobileMenu = function () {
+	btnOpen.setAttribute("aria-expanded", "true");
+	menuTopNav.removeAttribute("inert");
+};
+
+const openCloseMenu = function () {
+	btnOpen.setAttribute("aria-expanded", "false");
+	menuTopNav.setAttribute("inert", "");
+};
 
 btnOpen.addEventListener("click", openMobileMenu);
 btnClose.addEventListener("click", openCloseMenu);
+breakpoint.addEventListener("change", setupTopNav);
+
+setupTopNav();
