701ff7091a449bbbceab2b4e02e523838e79813d
Chris <chris79gp@outlook.com>
2024-12-24 18:27:03 +0000

    Feat: add functionality open and close mobile menu

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import "./app/scss/style.scss";
+
+console.log("Hello world!");
+
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+
+const openMobileMenu = () => btnOpen.setAttribute("aria-expanded", "true");
+const openCloseMenu = () => btnOpen.setAttribute("aria-expanded", "false");
+
+btnOpen.addEventListener("click", openMobileMenu);
+btnClose.addEventListener("click", openCloseMenu);
